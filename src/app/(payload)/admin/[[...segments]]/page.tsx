1467b5c8767131980edaecafd9416f53e3fb6e36
Saurabh <saurabh@uncover.co.in>
2026-04-04 08:15:22 +0000

    Fix admin page types - use any for props

--- src/app/(payload)/admin/[[...segments]]/page.tsx
+++ src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,16 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { DefaultTemplate } from '@payloadcms/next/templates';
 import { importMap } from '../importMap';
 import configPromise from '../../../../payload.config';
 
 export { generatePageMetadata as generateMetadata } from '@payloadcms/next/views';
 
-type Args = {
-  params: Promise<{ segments: string[] }>;
-  searchParams: Promise<{ [key: string]: string | string[] }>;
-};
-
-const Page = async ({ params, searchParams }: Args) => {
-  return <DefaultTemplate config={configPromise} importMap={importMap} params={params} searchParams={searchParams} />;
+const Page = async (props: any) => {
+  return <DefaultTemplate config={configPromise} importMap={importMap} {...props} />;
 };
 
 export default Page;
